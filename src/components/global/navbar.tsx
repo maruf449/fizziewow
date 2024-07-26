8457eee34fb5911046e56d6e51184b9e65328094
maruf449 <zihadmaruf@gmail.com>
2024-07-26 17:37:27 +0600

    initial commit

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -22,26 +22,7 @@ const Navbar = async (props: Props) => {
         <p className="text-3xl font-bold">zie</p>
       </aside>
       <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
-        <ul className="flex items-center gap-4 list-none">
-          <li>
-            <Link href="#">Products</Link>
-          </li>
-          <li>
-            <Link href="#">Pricing</Link>
-          </li>
-          <li>
-            <Link href="#">Clients</Link>
-          </li>
-          <li>
-            <Link href="#">Resources</Link>
-          </li>
-          <li>
-            <Link href="#">Documentation</Link>
-          </li>
-          <li>
-            <Link href="#">Enterprise</Link>
-          </li>
-        </ul>
+        
       </nav>
       <aside className="flex items-center gap-4">
         <Link
@@ -53,6 +34,19 @@ const Navbar = async (props: Props) => {
             {user ? 'Dashboard' : 'Get Started'}
           </span>
         </Link>
+
+        <Link
+          href="/workflows"
+          className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+        >
+
+        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#009dff_0%,#393BB2_50%,#009dff_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-800 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            WorkFlow
+          </span>
+        
+        </Link>
+
         {user ? <UserButton afterSignOutUrl="/" /> : null}
         <MenuIcon className="md:hidden" />
       </aside>
